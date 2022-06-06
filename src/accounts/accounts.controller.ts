1a6d5bd9922ebf478ede60009c0a2b7ac94e0c7b
Igor Silva <osilvaigor@gmail.com>
2022-06-06 00:18:18 -0300

    feat: add api routes

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,9 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('accounts')
 export class AccountsController {
   @Get()
   accounts() {
+    return 'Not implemented yet!';
+  }
+
+  @Get(':id/balance')
+  accountBalance(@Param('id') id: string) {
+    return 'Not implemented yet!';
+  }
+
+  @Get(':id/statement')
+  accountStatement(@Param('id') id: string) {
+    return 'Not implemented yet!';
+  }
+
+  @Post()
+  account(@Body() body) {
+    return 'Not implemented yet!';
+  }
+
+  @Post(':id/transfer')
+  transferBetweenAccounts(@Param('id') id: string, @Body() body) {
+    return 'Not implemented yet!';
+  }
+
+  @Patch(':id/deposit')
+  accountDeposit(@Param('id') id: string, @Body() body) {
+    return 'Not implemented yet!';
+  }
+
+  @Patch(':id/withdraw')
+  accountWithdraw(@Param('id') id: string, @Body() body) {
     return 'Not implemented yet!';
   }
 }
