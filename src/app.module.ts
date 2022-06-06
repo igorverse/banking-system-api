9c0abd83c4fba038cb440772e654d0da283b0013
Igor Silva <osilvaigor@gmail.com>
2022-06-06 13:08:18 -0300

    feat: add accounts dedicated nestjs module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AccountsController } from './accounts/accounts.controller';
-import { AccountsService } from './accounts/accounts.service';
+import { AccountsModule } from './accounts/accounts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, AccountsController],
-  providers: [AppService, AccountsService],
+  imports: [AccountsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
