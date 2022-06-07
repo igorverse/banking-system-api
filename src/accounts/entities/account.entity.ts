e458b7964518f6afcfbcad37ccc8ab75f5c15934
Igor Silva <osilvaigor@gmail.com>
2022-06-07 10:02:41 -0300

    feat: add typeorm entity

--- src/accounts/entities/account.entity.ts
+++ src/accounts/entities/account.entity.ts
@@ -1,5 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Account {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   name: string;
+
+  @Column()
   balance: number;
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [AccountsModule],
+  imports: [
+    AccountsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'mysecretpass',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
