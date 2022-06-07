fbd1ff7c7ecc450503c9f2382b6754acf0701531
Igor Silva <osilvaigor@gmail.com>
2022-06-07 13:29:38 -0300

    feat: add repository pattern and some business logic

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,22 +1,28 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateAccountDto } from './dto/create-account.dto';
+import { CreateTransferDto } from './dto/create-transfer.dto';
+import { UpdateAccountDto } from './dto/update-account.dto';
 import { Account } from './entities/account.entity';
 
 @Injectable()
 export class AccountsService {
-  private accounts: Account[] = [
-    {
-      id: '31a06b16-e09c-11ec-9d64-0242ac120002',
-      name: 'Satoshi Nakamoto',
-      balance: 420000000000,
-    },
-  ];
+  constructor(
+    @InjectRepository(Account)
+    private readonly accountRepository: Repository<Account>,
+  ) {}
 
   findAccounts() {
-    return this.accounts;
+    return this.accountRepository.find();
   }
 
-  findAccount(id: string) {
-    const account = this.accounts.find((account) => account.id === id);
+  async findAccount(id: string) {
+    const account = await this.accountRepository.findOne({ where: { id } });
 
     if (!account) {
       throw new NotFoundException(`Account #${id} not found!`);
@@ -25,37 +31,109 @@ export class AccountsService {
     return account;
   }
 
-  findBallance(id: string) {
-    const { balance } = this.findAccount(id);
+  async findBallance(id: string) {
+    const account = await this.findAccount(id);
 
-    return balance;
+    const accountBalance = {
+      name: account.name,
+      balance: account.balance,
+    };
+
+    return accountBalance;
   }
 
   findStatement(id: string) {
     return 'Not implemented yet!';
   }
 
-  createAccount(createAccountDto: any) {
-    this.accounts.push(createAccountDto);
+  createAccount(createAccountDto: CreateAccountDto) {
+    const account = this.accountRepository.create(createAccountDto);
+
+    return this.accountRepository.save(account);
   }
 
-  makeATransfer(id: string, createTransferDto: any) {
-    return 'Not implemented yet!';
-  }
+  async makeATransfer(id: string, createTransferDto: CreateTransferDto) {
+    const transferAccount = await this.findAccount(id);
 
-  makeADeposit(id: string, updateAccountDto: any) {
-    const account = this.findAccount(id);
+    const { idAccountToTransfer, value } = createTransferDto;
 
-    if (account) {
-      return 'Not implemented yet!';
+    if (id === idAccountToTransfer) {
+      throw new BadRequestException("You can't transfer to your own account!");
     }
+
+    if (value >= transferAccount.balance) {
+      throw new BadRequestException(
+        'You balance need to be greater or equal than the value of transfer',
+      );
+    }
+
+    const transferedAccount = await this.findAccount(idAccountToTransfer);
+
+    const newTransferAccountBalance = transferAccount.balance - value;
+    const newTransferedAccountBalance = transferedAccount.balance + value;
+
+    const transferAccountWithNewBalance = await this.accountRepository.preload({
+      id,
+      balance: newTransferAccountBalance,
+    });
+
+    const transferedAccountWithNewBalance =
+      await this.accountRepository.preload({
+        id: idAccountToTransfer,
+        balance: newTransferedAccountBalance,
+      });
+
+    if (!transferedAccount || !transferAccount) {
+      throw new NotFoundException(
+        'Some or both account were not found! Invalid ID!',
+      );
+    }
+
+    await this.accountRepository.save(transferAccountWithNewBalance);
+    await this.accountRepository.save(transferedAccountWithNewBalance);
+
+    return `Account #${id} tranfered $${value} to account #${idAccountToTransfer}`;
   }
 
-  makeAWithdraw(id: string, updateAccountDto: any) {
-    const account = this.findAccount(id);
+  async makeADeposit(id: string, updateAccountDto: UpdateAccountDto) {
+    const account = await this.findAccount(id);
+    const { value } = updateAccountDto;
 
-    if (account) {
-      return 'Not implemented yet!';
+    const newBalance = account.balance + value;
+
+    const accountWithNewBalance = await this.accountRepository.preload({
+      id,
+      balance: newBalance,
+    });
+
+    if (!account) {
+      throw new NotFoundException(`Account #${id} not found!`);
     }
+
+    return this.accountRepository.save(accountWithNewBalance);
+  }
+
+  async makeAWithdraw(id: string, updateAccountDto: UpdateAccountDto) {
+    const account = await this.findAccount(id);
+    const { value } = updateAccountDto;
+
+    if (value > account.balance) {
+      throw new BadRequestException(
+        "You don't have enough balance to request this withdraw! Check your balance!",
+      );
+    }
+
+    const newBalance = account.balance - value;
+
+    const accountWithNewBalance = await this.accountRepository.preload({
+      id,
+      balance: newBalance,
+    });
+
+    if (!account) {
+      throw new NotFoundException(`Account #${id} not found!`);
+    }
+
+    return this.accountRepository.save(accountWithNewBalance);
   }
 }
